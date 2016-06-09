50e91613b9742ae1a8b33778ac1d1714a6acf9e8
JCash <mwesterdahl76@gmail.com>
2016-06-09 23:40:36 +0200

    Added math lib

--- compile_clang.sh
+++ compile_clang.sh
@@ -1,2 +1,2 @@
 mkdir -p build
-clang -o build/main -g -O0 -m64 -std=c99 -Wall -Weverything -Wno-float-equal -pedantic src/main.c
+clang -o build/main -g -O0 -m64 -std=c99 -Wall -Weverything -Wno-float-equal -pedantic -lm src/main.c
