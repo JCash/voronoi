3ca9a239b4a9236da3285045ac6600408f1d09d8
JCash <mwesterdahl76@gmail.com>
2016-12-31 03:42:24 +0100

    test

--- compile_cl.bat
+++ compile_cl.bat
@@ -2,28 +2,28 @@ echo off
 
 if NOT DEFINED VCINSTALLDIR (
     if exist "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" (
-        call "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat"
+        call "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" amd64
         echo "USING VISUAL STUDIO 15"
     )
 )
 
 if NOT DEFINED VCINSTALLDIR (
     if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
-        call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+        call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
         echo "USING VISUAL STUDIO 14"
     )
 )
 
 if NOT DEFINED VCINSTALLDIR (
     if exist "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" (
-        call "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat"
+        call "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" amd64
         echo "USING VISUAL STUDIO 13"
     )
 )
 
 if NOT DEFINED VCINSTALLDIR (
     if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
-        call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+        call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
         echo "USING VISUAL STUDIO 12"
     )
 )
