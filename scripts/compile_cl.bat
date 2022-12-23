efa2605d7bef26ab4938d8f4e9d80dfe13c554de
JCash <mwesterdahl76@gmail.com>
2022-12-23 23:33:14 +0100

    win build fixes

--- scripts/compile_cl.bat
+++ scripts/compile_cl.bat
@@ -2,7 +2,7 @@ echo off
 
 mkdir build
 
-cl.exe /nologo /O2 /D_CRT_SECURE_NO_WARNINGS /c -Fobuild/stb_wrapper.obj src/stb_wrapper.c
-cl.exe /nologo /O2 /D_CRT_SECURE_NO_WARNINGS /W4 build/stb_wrapper.obj src\main.c /link /out:build/main.exe
+cl.exe /nologo /O2 /D_CRT_SECURE_NO_WARNINGS /c -Fobuild\stb_wrapper.obj src\stb_wrapper.c
+cl.exe /nologo /O2 /D_CRT_SECURE_NO_WARNINGS /W4 build\stb_wrapper.obj src\main.c /link /out:build\main.exe
 
 del *.obj
